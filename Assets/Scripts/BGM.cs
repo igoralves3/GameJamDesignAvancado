2ad0b3d8dce64172d5712b7af6d44f48fc2e6d08
igoralves3 <80365559+igoralves3@users.noreply.github.com>
2025-10-31 17:53:15 -0300

    adicionando musica "70's Cop Chase"

---
+++ Assets/Scripts/BGM.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BGM : MonoBehaviour
+{
+
+    public static BGM instance;
+
+    void Awake()
+    {
+        if (instance != null)
+        {
+            Destroy(gameObject);
+        }
+        else
+        {
+            instance = this;
+            DontDestroyOnLoad(this.gameObject);
+        }
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
