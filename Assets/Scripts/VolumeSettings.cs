d7aa876f38e4275c4b14b8fc9a2547774e27cb6b
igoralves3 <80365559+igoralves3@users.noreply.github.com>
2025-10-31 15:14:48 -0300

    colocando audio mixer sem testar audio

---
+++ Assets/Scripts/VolumeSettings.cs
@@ -0,0 +1,33 @@
+
+using UnityEngine;
+using UnityEngine.Audio;
+using UnityEngine.UI;
+
+public class VolumeSettings : MonoBehaviour
+{
+
+    [SerializeField] private AudioMixer audioMixer;
+    //[SerializeField] private AudioMixer sfxAudioMixer;
+    [SerializeField] private Slider bgmSlider;
+    [SerializeField] private Slider sfxSlider;
+
+    public void SetBGMVolume()
+    {
+        float volume = bgmSlider.value;
+        audioMixer.SetFloat("bgm",Mathf.Log10(volume)*20);
+    }
+
+    public void SetSFXVolume()
+    {
+        float volume = sfxSlider.value;
+        audioMixer.SetFloat("sfx", Mathf.Log10(volume) * 20);
+    }
+
+    void Start()
+    {
+        SetBGMVolume();
+        SetSFXVolume();
+    }
+
+
+}
