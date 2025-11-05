1e8183b917f9585d5b9d0c1376400b3d73a3b5df
igoralves3 <80365559+igoralves3@users.noreply.github.com>
2025-11-05 17:59:17 -0300

    adicionando cor no fundo das demais fases

--- Assets/Scripts/PlayerCamera.cs
+++ Assets/Scripts/PlayerCamera.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class PlayerCamera : MonoBehaviour
 {
@@ -8,6 +9,8 @@ public class PlayerCamera : MonoBehaviour
     public Transform target;             // Jogador
     public float smoothSpeed = 0.125f;
     public Vector3 offset;               // Offset da câmera em relação ao jogador
+
+    public Camera camera;
 
     // Limites da câmera
     public float minX, maxX, minY=-4, maxY=13;
@@ -32,7 +35,25 @@ public class PlayerCamera : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        camera = GetComponent<Camera>();
+
+        var currentSceneName = SceneManager.GetActiveScene().name;
+
+        if (currentSceneName == "Stage0" || currentSceneName == "Stage1")
+        {
+            camera.backgroundColor = new Color(0.1960784f, 0.8039216f, 0.1960784f, 1f);// Color.limeGreen;
+        }else if (currentSceneName == "Stage2" || currentSceneName == "Stage4")
+        {
+            camera.backgroundColor = new Color(0.5294118f, 0.8078432f, 0.9215687f, 1f);// Color.skyBlue;
+        }
+        else if (currentSceneName == "Stage3")
+        {
+            camera.backgroundColor = new Color(0.1882353f, 0.682353f, 0.7490196f, 1f);// Color.softblue;
+        }
+        else if (currentSceneName == "Stage5" || currentSceneName == "Stage6")
+        {
+            camera.backgroundColor = new Color(0.9019608f, 0.9019608f, 0.9803922f, 1f);// Color.lavender;
+        }
     }
 
     // Update is called once per frame
